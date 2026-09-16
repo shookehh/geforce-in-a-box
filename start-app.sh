3047c75a8177563f50cd0f2acae99f5a998d56ef
shookehh <99718379+shookehh@users.noreply.github.com>
2026-09-16 12:53:26 +0800

    Delete start-app.sh

--- start-app.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-fluxbox &
-sleep 1
-exec google-chrome-stable \
-  --no-sandbox \
-  --disable-dev-shm-usage \
-  --no-first-run \
-  --start-maximized \
-  "https://play.geforcenow.com/mall"
